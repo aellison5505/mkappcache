89daa2e9ca2dd2612a7b7d7a5dd7573c38272dbd
aellison5505 <aellison@mobilewebap.net>
2017-03-14 17:59:56 -0400

    complete file build output to screen
    Signed-off-by: aellison5505 <aellison@mobilewebap.net>

--- bin/index.js
+++ bin/index.js
@@ -1,27 +1,43 @@
 #!/usr/bin/env node
 
-const root = 'dist';
+var root = 'dist';
 var remove = 'dist/';
 var network = ['index.html'];
+var fallbacks = '{"fallback":[{"file":"/","fallto":"index.html"}]}';
+var exfile = '';
 
 const fs = require('fs');
 const crypto = require('crypto');
-const async = require('async');
-
+let strFile = [];
 
 console.log('CACHE MANIFEST');
+strFile.push('CACHE MANIFEST');
+
+
 
 mknetwork = (files) => {
     console.log('NETWORK:')
+    strFile.push('NETWORK:');
     files.forEach(file => {
         console.log(file);
+        strFile.push(file);
     });
 
 }
 
+mkfallback = (file) => {
+  var obj = JSON.parse(file);
+  console.log('FALLBACK:');
+  strFile.push('FALLBACK:');
+  obj.fallback.forEach(key =>{
+    console.log(key.file+' '+key.fallto);
+    strFile.push(key.file+' '+key.fallto);
+ });
+}
 
 
-readfiles = (folder,callback) => {
+
+readfiles = (folder) => {
     fs.readdir(folder, (err, files) => {
         files.forEach(file => {
             var fullfile = folder + '/' + file;
@@ -30,16 +46,30 @@ readfiles = (folder,callback) => {
                     fs.readFile(fullfile, (err, buf) => {
                         var hash = crypto.createHash('md5');
                         hash.update(buf);
-                        console.log(fullfile.replace(remove, '') + '/' + file);
-                        console.log('# ' + hash.digest('hex'));
+                        var strTmp1 = fullfile.replace(remove, '');
+                        var strTmp2 = '# ' + hash.digest('hex');
+                        console.log(strTmp1);
+                        strFile.push(strTmp1);
+                        console.log(strTmp2);
+                        strFile.push(strTmp2);
                     });
                 } else {
-                    callback(folder + '/' + file,readfiles);
+                    readfiles(folder + '/' + file);
                 }
             });
         });
     });
 
 }
-
-readfiles(root, readfiles);
+let count=0;
+readfiles(root);
+let timer = setInterval(()=>{
+  if(count===strFile.length){
+    clearInterval(timer);
+    mknetwork(network);
+    mkfallback(fallbacks);
+//    console.log(strFile);
+  }else{
+    count = strFile.length;
+  }
+},250);
