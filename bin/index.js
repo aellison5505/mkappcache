549b54634bc7b354fa61edb273ef57176bef76b8
aellison5505 <aellison@mobilewebap.net>
2017-03-14 15:44:17 -0400

    broken
    Signed-off-by: aellison5505 <aellison@mobilewebap.net>

--- bin/index.js
+++ bin/index.js
@@ -1,34 +1,45 @@
 #!/usr/bin/env node
 
-const testFolder = './dist/';
+const root = 'dist';
+var remove = 'dist/';
+var network = ['index.html'];
+
 const fs = require('fs');
 const crypto = require('crypto');
+const async = require('async');
+
 
 console.log('CACHE MANIFEST');
 
-readfiles = (folder) => {
-  subfolder = [];
+mknetwork = (files) => {
+    console.log('NETWORK:')
+    files.forEach(file => {
+        console.log(file);
+    });
+
+}
+
+
+
+readfiles = (folder,callback) => {
     fs.readdir(folder, (err, files) => {
         files.forEach(file => {
-            fs.stat(folder + '/' + file, (err, stats) => {
+            var fullfile = folder + '/' + file;
+            fs.stat(fullfile, (err, stats) => {
                 if (!stats.isDirectory()) {
-                    fs.readFile(folder + '/' + file, (err, buf) => {
+                    fs.readFile(fullfile, (err, buf) => {
                         var hash = crypto.createHash('md5');
                         hash.update(buf);
-                        console.log(file);
+                        console.log(fullfile.replace(remove, '') + '/' + file);
                         console.log('# ' + hash.digest('hex'));
                     });
-                }else{
-                  readfiles(folder+'/'+file);
+                } else {
+                    callback(folder + '/' + file,readfiles);
                 }
             });
         });
     });
-    console.log(subfolder);
-    subfolder.forEach((sub, index) => {
-      readfiles(folder+'/'+sub);
-    });
+
 }
 
-
-readfiles(testFolder);
+readfiles(root, readfiles);
