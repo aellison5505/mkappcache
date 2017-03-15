d6ef04db95b472958b3ddd8c73e2bb87addd00ce
aellison5505 <aellison@mobilewebap.net>
2017-03-14 23:24:28 -0400

    added removal meow cli commands started need rest of commands config file
    Signed-off-by: aellison5505 <aellison@mobilewebap.net>

--- bin/index.js
+++ bin/index.js
@@ -2,18 +2,57 @@
 
 var root = 'dist';
 var remove = 'dist/';
-var network = ['index.html'];
+var network = ['*'];
 var fallbacks = '{"fallback":[{"file":"/","fallto":"index.html"}]}';
-var exfile = ['app.appcache'];
+var exfile = ['app.appcache','service-worker.js'];
+var fileOut = 'app.appcache';
 
 const fs = require('fs');
 const crypto = require('crypto');
-let strFile = [];
+const meow = require('meow');
+const updateNotifier = require('update-notifier');
+const pkg = require('../package.json');
 
-////console.log('CACHE MANIFEST');
+updateNotifier({pkg: pkg}).notify();
+
+let strFile = [];
+//console.log('CACHE MANIFEST');
 strFile.push('CACHE MANIFEST');
 
+const cli = meow(`
+    Usage
+      $ mkappcache outFile
+          default: app.appcache
+    Options
+      -c config, --config=path to config file
+            default config same directory from call
+      -h, --help
 
+    Examples
+      $ mkappcache app.appcache -c=.appcache-config
+`, {
+    alias: {
+        c: 'config',
+        h: 'help',
+        v:'version'
+    }
+});
+
+phraseCli = () => {
+  var flags = cli.flags;
+  if(cli.input[0]){
+    fileOut=cli.input[0];
+  }
+  if(flags.config){
+    console.log(flags.config);
+  }
+
+  //console.log(cli);
+
+}
+
+phraseCli();
+//return;
 
 mknetwork = (files) => {
     //console.log('NETWORK:')
@@ -39,37 +78,55 @@ mkfallback = (file) => {
 
 readfiles = (folder) => {
     fs.readdir(folder, (err, files) => {
-        files.forEach(file => {
-            var fullfile = folder + '/' + file;
-            fs.stat(fullfile, (err, stats) => {
-                if (!stats.isDirectory()) {
-                    fs.readFile(fullfile, (err, buf) => {
-                        var hash = crypto.createHash('md5');
-                        hash.update(buf);
-                        var strTmp1 = fullfile.replace(remove, '');
-                        var strTmp2 = '# ' + hash.digest('hex');
-                        //console.log(strTmp1);
-                        strFile.push(strTmp1);
-                        //console.log(strTmp2);
-                        strFile.push(strTmp2);
-                    });
-                } else {
-                    readfiles(folder + '/' + file);
-                }
+        sortEx(files, (ret) => {
+            ret.forEach(file => {
+                var fullfile = folder + '/' + file;
+                fs.stat(fullfile, (err, stats) => {
+                    if (!stats.isDirectory()) {
+                        fs.readFile(fullfile, (err, buf) => {
+                            var hash = crypto.createHash('md5');
+                            hash.update(buf);
+                            var strTmp1 = fullfile.replace(remove, '');
+                            var strTmp2 = '# ' + hash.digest('hex');
+                            //console.log(strTmp1);
+                            strFile.push(strTmp1);
+                            //console.log(strTmp2);
+                            strFile.push(strTmp2);
+                        });
+                    } else {
+                        readfiles(folder + '/' + file);
+                    }
+                });
             });
         });
     });
-
 }
 
 outFile = (fileArray) => {
     var strOut = fileArray.join('\n');
     console.log(strOut);
-    fs.writeFile(root + '/app.appcache', strOut, function(err) {
+    fs.writeFile(root + '/' + fileOut, strOut, function(err) {
         if (err) {
             return console.log("Error writing file: " + err);
         }
     });
+}
+
+sortEx = (inArray, callback) => {
+    var tmpArray = [];
+    inArray.forEach(file => {
+        //exfile.forEach(ex => {
+          for(i=0;i<exfile.length;i++){
+            if (file == exfile[i]) {
+              console.log('found'+exfile[i]);
+              file='';
+            }
+          }
+            if(file!=''){
+                tmpArray.push(file);
+            }
+    });
+    callback(tmpArray.sort());
 }
 
 let count = 0;
